48d6dbfe300cf35952bb3839da7d580690aeb475
Bobby Zhang <137586907+bobbyethwaterloo2023@users.noreply.github.com>
2023-06-24 21:34:26 -0400

    Implemented sismo button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,58 @@ const inter = Inter({ subsets: ['latin'] })
 import {  useState } from 'react'
 import { Disclosure } from '@headlessui/react'
 import SignIn from '@/components/SignIn'
+import {
+  AuthType,
+  ClaimType,
+  SismoConnectButton,
+  SismoConnectConfig,
+  SismoConnectResponse,
+} from "@sismo-core/sismo-connect-react";
+
+/* ***********************  Sismo Connect Config *************************** */
+const sismoConnectConfig = {
+  appId: "0xf4977993e52606cfd67b7a1cde717069",
+  vault: {
+    // For development purposes insert the identifier that you want to impersonate here
+    // Never use this in production
+    impersonate: [
+      "dhadrien.sismo.eth",
+      "github:dhadrien",
+      "twitter:dhadrien_",
+    ],
+  },
+};
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState();
+  const [error, setError] = useState();
 
-  
+ async function onSismoConnectResponse(response) {
+  setLoading(true);
+  try {
+    const res = await fetch("/api/sismo", {
+      method: "POST",
+      body: JSON.stringify(response),
+    });
+    console.log('res 42 - ', res)
+    if (!res.ok) {
+      const error = await res.json();
+      setError(error);
+      return;
+    }
+    const user = await res.json();
+    console.log('48: ', user)
+    setUser(user);
+  } catch (err) {
+    console.log('51 - ')
+    setError(err.message);
+  } finally {
+    setLoading(false);
+  }
+}
+
   return (
     <>
       <div className="min-h-full">
@@ -47,6 +94,44 @@ export default function Home() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white justify-center font-bold py-2 px-4 rounded-full mx-auto text-xl" onClick={() => setShowModal(true)}>
               Sign In
             </button>
+            <SismoConnectButton
+              // the client config created
+              config={sismoConnectConfig}
+              // the auth request we want to make
+              // here we want the proof of a Sismo Vault ownership from our users
+              auths={[
+                { authType: AuthType.VAULT },
+                { authType: AuthType.EVM_ACCOUNT },
+              ]}
+              claims={[
+                // we ask the user to prove that he has a gitcoin passport with a score above 15
+                // https://factory.sismo.io/groups-explorer?search=0x1cde61966decb8600dfd0749bd371f12
+                {
+                  groupId: "0x1cde61966decb8600dfd0749bd371f12",
+                  claimType: ClaimType.GTE,
+                  value: 15,
+                },
+                // we ask the user to prove that he is part of the Sismo Contributors group and selectively prove its level
+                // https://factory.sismo.io/groups-explorer?search=0xe9ed316946d3d98dfcd829a53ec9822e
+                {
+                  groupId: "0xe9ed316946d3d98dfcd829a53ec9822e",
+                  isSelectableByUser: true,
+                },
+                // we optionally ask the user to prove that he is following Sismo on Lens
+                // https://factory.sismo.io/groups-explorer?search=0xabf3ea8c23ff96893ac5caf4d2fa7c1f
+                {
+                  groupId: "0xabf3ea8c23ff96893ac5caf4d2fa7c1f",
+                  isOptional: true,
+                },
+              ]}
+              // we ask the user to sign a message
+              signature={{ message: "hello", isSelectableByUser: true }}
+              // onResponseBytes calls a 'setResponse' function with the responseBytes returned by the Sismo Vault
+              onResponse={(response) => {
+                onSismoConnectResponse(response);
+              }}
+              verifying={loading}
+            />
             {showModal && (
               <div className="bg-white border-2 border-blue-500 fixed top-0 left-0 w-full h-full flex justify-center items-center">
                 <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 space-y-2">
